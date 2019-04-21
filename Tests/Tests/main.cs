8bc9ec1469e625e100e5438c49db89076ecd3d4c
w1jtoo <wanadooht@gmail.com>
2019-04-21 18:09:00 +0500

    Hello Tests
    Add test project 
    Updating of core frameworks 
    Main Tests

---
+++ Tests/Tests/main.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Runtime.CompilerServices;
+using NUnit.Framework;
+using GlLib;
+using GlLib.Common;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            Console.WriteLine("Testing starts");
+        }
+        [Test]
+        public void GameStart()
+        {
+            Assert.DoesNotThrow(() => Core.Main(new string[0]));
+        }
+        [Test]
+        public void MainCheck()
+        {
+            Assert.IsTrue(true);
+        }
+
+    }
+}
