0be3c052bfb53870e7e62841c0b699a7485df4d3
Jimmy <wanadooht@gmail.com>
2019-05-08 11:45:47 +0500

    WinForms <3
    OnFrame updating of chunk editor 
    Mouse checker 
    Some fixes: Refactor e.t.c

--- MapEditor/MapEditor/MapEditor.cs
+++ MapEditor/MapEditor/MapEditor.cs
@@ -6,7 +6,7 @@ using System.Windows.Forms;
 
 namespace MapEditor
 {
-    static class Program
+    static class MapEditor
     {
         /// <summary>
         /// Главная точка входа для приложения.
@@ -16,7 +16,7 @@ namespace MapEditor
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Window());
         }
     }
 }
