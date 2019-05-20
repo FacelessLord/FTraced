48ee0bbf6aeefef284692fe5b6717c70babdb15a
faceless <skyres21@yandex.ru>
2019-05-20 16:43:14 +0500

    Slime naming fix

--- AirGame/Client/Graphic/Renderers/SlimeRenderer.cs
+++ AirGame/Client/Graphic/Renderers/SlimeRenderer.cs
@@ -25,8 +25,7 @@ namespace GlLib.Client.Graphic.Renderers
 
         public override void Setup(Entity _p)
         {
-
-            var idle = new TextureLayout("slime/smile_idle.png", 10, 1);
+            var idle = new TextureLayout("slime/slime_idle.png", 10, 1);
             var walk = new TextureLayout("slime/slime_waiting.png", 7, 1);
 
             idleSprite = new LinearSprite(idle, 10, 30);
